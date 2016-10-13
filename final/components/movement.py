757f6be1b79334b480f86fb0c6b1970395cea2d0
Lukas Zapolskas <lukas.zapolskas@gmail.com>
2016-10-13 10:08:17 +0400

    Removal of experimental final project

--- final/components/movement.py
+++
@@ -1,7 +0,0 @@
-class Velocity:
-    """Velocity component for all dynamic objects."""
-
-    def __init__(self, v_x=0, v_y=0):
-        """Create instance with defined velocity."""
-        self.v_x = v_x
-        self.v_y = v_y
--- final/constants.py
+++
@@ -1,7 +0,0 @@
-import os
-
-RESOLUTION = (1020, 720)
-FRAMERATE = 60
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), "static")
-IMAGE_ROOT = os.path.join(STATIC_ROOT, "img")
-MAP_ROOT = os.path.join(STATIC_ROOT, "maps")
