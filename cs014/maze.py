b9c797bded06fc57cd9212848a4a93aa9c7d9df6
Lukas Zapolskas <lukas.zapolskas@gmail.com>
2016-11-01 11:15:35 +0400

    First working version of the maze crawler

--- cs014/maze.py
+++ cs014/maze.py
@@ -1,10 +1,43 @@
 import helpers
 
 # Your job is to develop this function
+
+
 def find_path(board, row, col):
-    next_cell = [(row - 1, col), (row, col + 1), (row + 1, col), (row, col + 1)]
+    # Possible cells for movement. Limited to a
+    paths = [(row - 1, col), (row, col - 1),
+             (row + 1, col), (row, col + 1)]
+
+    next_cells = [cell for cell in paths if helpers.islegal(board, cell[
+                                                            0], cell[1])]
+
+    next_cells = [cell for cell in next_cells if board[
+        cell[0]][cell[1]] == "."]
+
+    victory = any(
+        list([victor for cell in next_cells if board[cell[0]][cell[1]] == "x"]))
+
+    print(next_cells)
+
+    if not victory:
+        if not len(next_cells):
+            return False
+        elif len(next_cells) == 1:
+            row, col = next_cells[0]
+            board[row][col] = "*"
+            find_path(board, row, col)
+        else:
+            for item in next_cells:
+                row, col = item
+                board[row][col] = "*"
+                find_path(board, item[0], item[1])
+    else:
+        return True
+
 
 board = helpers.get_board('board.txt')
+
+
 helpers.print_board(board)
 print('-' * 50)
 
