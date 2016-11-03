d9a3d54ccff2034b1a8dd78c356abd5e6600070e
Lukas Zapolskas <lukas.zapolskas@gmail.com>
2016-11-03 11:35:38 +0400

    Rock paper scissors via object

--- cs015/exer3.py
+++ cs015/exer3.py
@@ -1,10 +1,14 @@
 """Exercise 1.3: Rock-paper-scissors."""
+import random
 
 
 class Rock:
 
+    def __init__(self):
+        self.name = "rock"
+
     def __gt__(self, other):
-        if other.name != "paper":
+        if other.name == "paper":
             return True
         else:
             return False
@@ -68,3 +72,31 @@ class Scissors:
 
     def __str__(self):
         return self.name
+
+hands = [Rock(), Paper(), Scissors()]
+
+stats = {
+    "wins": 0,
+    "losses": 0,
+    "ties": 0
+}
+
+for i in range(1, 10):
+    player1 = random.choice(hands)
+    player2 = random.choice(hands)
+
+    print("Player 1", player1)
+    print("Player 2", player2)
+    print(player1 > player2)
+    print(player1 < player2)
+    print(player1 == player2)
+    
+    if player1 > player2:
+        stats['wins'] += 1
+    elif player1 < player2:
+        stats['losses'] += 1
+    else:
+        stats['ties'] += 1
+
+for key, value in stats.items():
+    print(key.title(), value, sep="\t")
