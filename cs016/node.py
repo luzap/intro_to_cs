ff3b8b8689cc74ccb628060c69b805fe7c3106fd
Lukas Zapolskas <lukas.zapolskas@gmail.com>
2016-11-08 13:57:04 +0400

    Working binary tree implementation

--- cs016/node.py
+++ cs016/node.py
@@ -1,6 +1,6 @@
-"""Node implementation."""
+"""Custom binary tree node implementation."""
 
-# What to do when adding a value equal to the value of the node
+
 class Node:
 
     def __init__(self, value=None):
@@ -9,17 +9,56 @@ class Node:
         self.right = None
 
     def find(self, value):
-        if self.value == valeu:
+        """Recursive search for nodes containing a given value."""
+        if self.value == value:
             return True
-        elif self.value < value:
-            return self.left.find(value)
         else:
-            return self.right.find(value)
+            if self.value > value:
+                if not isinstance(self.left, Node):
+                    return False
+                else:
+                    return self.left.find(value)
+            elif self.value < value:
+                if not isinstance(self.right, Node):
+                    return False
+                else:
+                    return self.right.find(value)
 
     def add(self, value):
-        if self.value is None:
+        """Adding to binary tree based on comparison first Node. There are only
+        three possibilities for the values of any individual node. Either they
+        are greater than, less than or equal to the value given.
+
+        :param value: object
+        :returns: None
+        """
+
+        if self.value is None:  # If not initialized, initializes
             self.value = value
-        elif self.value < value:
-            self.left.add(value)
         else:
-            pass
+            if self.value > value:
+                # Checks whether or not the left node is a 
+                # Node object
+                if not isinstance(self.left, Node):
+                    self.left = Node(value=value)
+                else:
+                    self.left.add(value)
+            elif self.value < value:
+                if not isinstance(self.right, Node):
+                    self.right = Node(value=value)
+                else:
+                    self.left.add(value)
+
+if __name__ == "__main__":
+    node = Node()
+    node.add(6)
+    node.add(2)
+    node.add(5)
+    node.add(7)
+    print(node.find(6))
+    print(node.find(2))
+    print(node.find(5))
+    print(node.find(7))
+    print(node.find(4))
+    print(node.find(3))
+    print(node.find(10))
