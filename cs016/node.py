09873f0e2f38cae085881f28bcff48d0466f6f6a
Lukas Zapolskas <lukas.zapolskas@gmail.com>
2016-11-08 11:14:46 +0400

    Basic Node

--- cs016/node.py
+++ cs016/node.py
@@ -1,1 +1,25 @@
 """Node implementation."""
+
+# What to do when adding a value equal to the value of the node
+class Node:
+
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def find(self, value):
+        if self.value == valeu:
+            return True
+        elif self.value < value:
+            return self.left.find(value)
+        else:
+            return self.right.find(value)
+
+    def add(self, value):
+        if self.value is None:
+            self.value = value
+        elif self.value < value:
+            self.left.add(value)
+        else:
+            pass
